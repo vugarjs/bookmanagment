fb90feb9e9d3197602f91fb213016bfbd514fcf3
Vuqar <132734050+vugarjs@users.noreply.github.com>
2026-08-10 17:44:45 +0400

    added class

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1;
+
+public class Book : Product
+{
+    public string Genre;
+    public Book[] book;
+
+    public Book(string name, decimal price , int count , string genre):base(name,price,count)
+    {
+        Genre = genre;
+    }
+    public void AddBook()
+    {
+
+        Console.WriteLine("Enter lenght : ");
+        int bookLeght = Convert.ToInt32(Console.ReadLine());
+
+        book = new Book[bookLeght];
+
+
+
+        for (int i = 0; i < book.Length; i++)
+        {
+
+
+            book[i].Name = name;
+            book[i].Price = price;
+            book[i].Count = count;
+        }
+
+
+    }
+}
