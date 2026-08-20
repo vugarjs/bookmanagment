528002ec64d262be9b6f0634fee620219f7e6d12
Vuqar Suleymanov <mlolvuqar@gmail.com>
2026-08-20 13:21:17 +0400

    minors

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new();
+
+            Console.WriteLine("Enter name : ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter price : ");
+            decimal price = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter count : ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter genre : ");
+            string genre = Console.ReadLine();
+
+
+            Book book = new(name,price,count,genre);
+            book.AddBook();   
+        }
+    }
+}
